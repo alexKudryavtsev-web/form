9fb0fc79a7b644e87826c84b1abbb67e41b33f91
alexKudryavtsev-web <qwertyshurazh@gmail.com>
2020-06-10 02:21:49 +0300

    Make Submission

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React from 'react'
 import './App.css'
 import Title from './components/Title/Title'
 import Form from './components/Form/Form'
+import { SubmissionError } from 'redux-form'
+
+const systemFio = [
+  'admin', 'manager', 'Kim Chen In'
+]
 
 function App() {
   function submit(values) {
+    if (systemFio.includes(values.fio.trim()))
+      throw new SubmissionError({
+        fio: 'This is a system fio'
+      })
+
     console.log('form', values)
   }
 
